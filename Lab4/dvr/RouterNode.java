a6628c0c28161391aaaf2304695067efc163d8e9
George Yildiz <geoyi478@student.liu.se>
2016-05-03 10:33:45 +0200

    lab4 ready for demonstration

--- Lab4/dvr/RouterNode.java
+++ Lab4/dvr/RouterNode.java
@@ -15,21 +15,22 @@ public class RouterNode {
 	private int[] routeThrough = new int[RouterSimulator.NUM_NODES];
 	private int[][] distanceVectors = new int[RouterSimulator.NUM_NODES][RouterSimulator.NUM_NODES];
 	private ArrayList<Integer> neighbours = new ArrayList<>();
-	//private HashMap<Integer, int[]> neighboursDistVectors = new HashMap<>();	
-	
 
-	//private HashMap<Integer, Integer> neighbourss = new HashMap<>();
-	
+	//Change here to activate/deactivate poisoned reverse
+	private boolean poisonedReverse = true;
+		
 
-  //--------------------------------------------------
+  /**
+	*	The constructor will initialize the distancevectors and our neighbours list
+	*/
   public RouterNode(int ID, RouterSimulator sim, int[] costs) {
     myID = ID;
     this.sim = sim;
     myGUI =new GuiTextArea("  Output window for Router #"+ ID + "  ");
     System.arraycopy(costs, 0, this.costs, 0, RouterSimulator.NUM_NODES);
 		
-
-		//initialize neighbours and who routing is done via.
+		//Set the distance vectors for the neighbours to all routers to infinity in the beggining
+		//Set the neighbours that each router have and who we route via (next hop).
 		for(int i = 0 ; i < RouterSimulator.NUM_NODES; i++){
 			if (costs[i] < RouterSimulator.INFINITY){
 				routeThrough[i] = i;
@@ -49,7 +50,9 @@ public class RouterNode {
 		printDistanceTable();
 		
   }
-
+	/**
+	* add the received updated and check if this leads to new routes, if so we notify the neighbours.
+	*/
   public void recvUpdate(RouterPacket pkt) {
  		System.arraycopy(pkt.mincost, 0, distanceVectors[pkt.sourceid], 0, RouterSimulator.NUM_NODES);
 		boolean update = changeMinCost();
@@ -65,7 +68,9 @@ public class RouterNode {
   }
   
 
-  //--------------------------------------------------
+  /**
+	*	Prints the distancetable, routes and costs for each router
+	*/
   public void printDistanceTable() {
 		StringBuilder sb = new StringBuilder();
 	  myGUI.println("Current table for " + myID +
@@ -75,27 +80,25 @@ public class RouterNode {
 		// build distanceTable
 		myGUI.println("Distancetable:");
 		printTableHeader();
-		
 		for(int nbr : neighbours){
 			if(nbr != myID){
 				sb.append(F.format("nbr  "+ nbr + " |", 9));
 				for(int j = 0; j < RouterSimulator.NUM_NODES; j++) {			
 					sb.append(F.format(distanceVectors[nbr][j], 5));
-				}	
-				myGUI.println(sb.toString());
-				sb.setLength(0);
+				}
+				sb.append("\n");	
 			}	
 		}	
 		//Build our distance vector and routes
-		myGUI.println(F.SPACES);
-		myGUI.println("Our distance vector and routes:");
+		sb.append(F.SPACES).append("\n").append("Our distance vector and routes:");
+		myGUI.println(sb.toString());
+		sb.setLength(0);
 		printTableHeader();
 		sb.append(F.format("cost   |", 9));
 		for(int c : costs){
 			sb.append(F.format(c,5));
 		}
-		myGUI.println(sb.toString());
-		sb.setLength(0);
+		sb.append("\n");
 		sb.append(F.format(" route  |", 9));
 		for(int v : routeThrough){
 			sb.append(F.format(v,5));
@@ -103,17 +106,34 @@ public class RouterNode {
 		myGUI.println(sb.toString());
 	}
 
-
+	/**
+	*	Iterate over the neighbours and send them a packet that has our new update.
+	*	If poisoned Reverse is enabled then we will set a cost to infinity where we route through the 
+	*	neighbour that we are about to send the update to.
+	*/
 	private void notifyNeighbours(){
 		for(int nbr : neighbours){
-			if(nbr != myID){
-				RouterPacket pkt = new RouterPacket(myID, nbr, costs);
-				sendUpdate(pkt);
+			if(nbr == myID) continue;
+			int[] tmpCosts = new int[RouterSimulator.NUM_NODES];
+			if(poisonedReverse){	
+				for(int i = 0; i < RouterSimulator.NUM_NODES; i++){
+					if(nbr == routeThrough[i]){
+						tmpCosts[i] = RouterSimulator.INFINITY;
+					}else{
+						tmpCosts[i] = costs[i];
+					}
+				}
+			}else{
+				System.arraycopy(costs, 0, tmpCosts, 0, RouterSimulator.NUM_NODES);
 			}
+			RouterPacket pkt = new RouterPacket(myID, nbr, tmpCosts);
+			sendUpdate(pkt);
 		}
 	}
 
-
+	/**
+	*	Help function for the printDistanceTable.
+	*/
 	private void printTableHeader(){
 		StringBuilder sb = new StringBuilder();
 		sb.append(F.format("dst |", 9));
@@ -159,8 +179,10 @@ public class RouterNode {
 		return update;
 	}
 	
-
-  //--------------------------------------------------
+	/**
+	*	add the received update for the link cost and check if this
+	* leads to new routes, if so we notify the neighbours.
+	*/	
   public void updateLinkCost(int dest, int newcost) {
 		distanceVectors[myID][dest] = newcost;
 		boolean update = false;;
